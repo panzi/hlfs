7c547bc4a327ce55f007e1b98ce47c5bb0fe9648
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2014-03-16 03:36:33 +0100

    initial commit

---
+++ src/hlfs.h
@@ -0,0 +1,27 @@
+/**
+ * hlfs - mount various valve archives (e.g. vpk)
+ * Copyright (C) 2014  Mathias Panzenböck <grosser.meister.morti@gmx.net>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef HLFS_HLFS_H
+#define HLFS_HLFS_H
+
+#include <HLLib.h>
+
+namespace HLFS {
+}
+
+#endif
